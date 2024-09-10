79314b7002cc1941dc7b2e2389f56f641f8cef97
andrearomeo04 <pisco004@gmail.com>
2024-09-11 00:37:13 +0200

    [UPD] esercizio finito

--- process.php
+++ process.php
@@ -1,6 +1,6 @@
 <?php 
-$text = $_GET['text'];
-$badWord = $_GET['badWord'];
+$text = $_POST['text'];
+$badWord = $_POST['badWord'];
 $censored = str_replace($badWord, '***', $text)
 ?>
 
@@ -13,6 +13,20 @@ $censored = str_replace($badWord, '***', $text)
     <title>Document</title>
 </head>
 <body>
-
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center mb-5">
+                <h1>RESULTS</h1>
+            </div>
+            <div class="col-12">
+                <h2>Testo censurato</h2>
+                <p><?php echo $censored?></p>
+            </div>
+            <div class="col-12 mb-5">
+                <h2>Testo non censurato</h2>
+                <p><?php echo $text?></p>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
